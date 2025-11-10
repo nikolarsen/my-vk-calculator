1eab1fd240ac2fe13a0ca733edd58baf5e3afb2f
nikolarsen <nikolarsen@yandex.ru>
2025-11-10 16:15:45 +0300

    Fix VK environment check

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client';
 import bridge from '@vkontakte/vk-bridge';
 import App from './App';
 
-// Инициализация VK Mini App
-bridge.send('VKWebAppInit');
+// Проверяем что мы в VK окружении
+if (typeof window !== 'undefined' && window.location.search.includes('vk_')) {
+  // Инициализация только в VK
+  bridge.send('VKWebAppInit')
+    .then(() => console.log('VK Mini App initialized'))
+    .catch((error) => console.log('VK init error:', error));
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
