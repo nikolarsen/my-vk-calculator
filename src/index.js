a9e09a8e0433218d71a590ac872f01435bf59efc
nikolarsen <nikolarsen@yandex.ru>
2025-11-10 16:32:42 +0300

    Remove VK Bridge for testing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import bridge from '@vkontakte/vk-bridge';
 import App from './App';
-
-// Проверяем что мы в VK окружении
-if (typeof window !== 'undefined' && window.location.search.includes('vk_')) {
-  // Инициализация только в VK
-  bridge.send('VKWebAppInit')
-    .then(() => console.log('VK Mini App initialized'))
-    .catch((error) => console.log('VK init error:', error));
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
